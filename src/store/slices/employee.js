6545e242de47e104c9de6111603956582f738907
Alexandre-Puech <alexandre.puech3@gmail.com>
2025-07-01 14:01:14 +0200

    feat: added state management for employee's data creation

---
+++ src/store/slices/employee.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+  firstName: "",
+  lastName: "",
+  dateOfBirth: "",
+  startDate: "",
+  department: "",
+  street: "",
+  city: "",
+  livingState: "",
+  zipCode: "",
+};
+
+export const employeeSlice = createSlice({
+  name: "employee",
+  initialState: initialState,
+  reducers: {
+    addEmployee(state, action) {
+      const {
+        firstName,
+        lastName,
+        dateOfBirth,
+        startDate,
+        department,
+        street,
+        city,
+        livingState,
+        zipCode,
+      } = action.payload;
+      state.firstName = firstName;
+      state.lastName = lastName;
+      state.dateOfBirth = dateOfBirth;
+      state.startDate = startDate;
+      state.department = department;
+      state.street = street;
+      state.city = city;
+      state.livingState = livingState;
+      state.zipCode = zipCode;
+    },
+  },
+});
