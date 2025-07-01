2bbb56567a05dd7bfd8d026206633116a2720e86
Alexandre-Puech <alexandre.puech3@gmail.com>
2025-07-01 14:05:43 +0200

    feat: added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import EmployeeList from "./pages/Employee-list";
+
 function App() {
-  return <></>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="employee-list" element={EmployeeList} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
